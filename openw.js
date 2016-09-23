5c2dd8d2d5783b4ab05af1acbceb48e33d07b2fb
i5ting <i5ting@126.com>
2016-09-24 02:26:11 +0800

    lazycommmit, please ignore

--- openw.js
+++ openw.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs')
 var debug = require('debug')('weide')
+var exec = require('child_process').exec;
 
 var argv = process.argv;
 argv.shift();
@@ -11,16 +12,11 @@ var current_path = process.cwd();
 
 var home = '/Applications/微信web开发者工具.app/'
 
-
 if(undefined !== process.env.WECHAT_IDE) {
   home = process.env.WECHAT_IDE + '/'
 }
 
-
-var exec = require('child_process').exec;
 exec('open ' + home, function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
     if (error !== null) {
         console.log('exec error: ' + error);
     }
